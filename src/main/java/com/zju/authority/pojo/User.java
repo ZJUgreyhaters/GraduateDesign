78222150de5981f588cf3bb1c11a674f0645ead6
wbchen <3150103635@zju.edu.cn>
2018-11-01 22:07:06 +0800

    # start work # create some basic pojo.

--- src/main/java/com/zju/authority/pojo/User.java
+++ src/main/java/com/zju/authority/pojo/User.java
@@ -9,5 +9,11 @@ package com.zju.authority.pojo;
  **/
 
 public class User {
+    private long uid;
+    private String realName;
+    private String nikeName;
+    private String Email;
+    private String phoneNumber;
+    private String password;
 
 }
--- src/main/java/com/zju/message/pojo/Message.java
+++ src/main/java/com/zju/message/pojo/Message.java
@@ -1,5 +1,7 @@
 package com.zju.message.pojo;
 
+import java.util.Date;
+
 /**
  * @ClassName: Message
  * @Description: TODO
@@ -9,5 +11,35 @@ package com.zju.message.pojo;
  **/
 
 public class Message {
+    private String message;
+    private Date date;
+    private long uid;
+
+    Message(){}
+
+    public Date getDate() {
+        return date;
+    }
+
+    public long getUid() {
+        return uid;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public void setUid(long uid) {
+        this.uid = uid;
+    }
+
 
 }
